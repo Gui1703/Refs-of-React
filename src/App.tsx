f2923251bb5ce1c4dfaf533c34dd539a8d27e1f9
Guilherme Raposo <gui.raposo36@gmail.com>
2022-03-20 17:56:54 -0300

    feat: created input component and used ForwardRefRenderFunction

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { FormEvent, useCallback, useRef } from "react";
+import Input from "./components/Input";
+
 function App() {
+  const nameInputRef = useRef<HTMLInputElement>(null);
+  const acceptTermsRef = useRef({ value: false });
+
+  const handleSubmit = useCallback((e: FormEvent) => {
+    e.preventDefault();
+
+    console.log(nameInputRef.current?.value);
+    console.log(acceptTermsRef.current.value);
+  }, []);
+
+  const handleAcceptTerms = useCallback(() => {
+    acceptTermsRef.current.value = !acceptTermsRef.current.value;
+  }, []);
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <form onSubmit={handleSubmit}>
+        <Input name="name" label="Nome Completo" ref={nameInputRef} />
+        <button type="button" onClick={handleAcceptTerms}>
+          Aceitar termos
+        </button>
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
