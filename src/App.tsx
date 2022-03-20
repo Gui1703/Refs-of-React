1416d26a49ddd4fecca0ad5f28ee3bc4b18e419c
Guilherme Raposo <gui.raposo36@gmail.com>
2022-03-20 18:26:27 -0300

    feat: finished app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { FormEvent, useCallback, useRef } from "react";
 import Input from "./components/Input";
+import Modal, { ModalHandles } from "./components/Modal";
 
 function App() {
   const nameInputRef = useRef<HTMLInputElement>(null);
   const acceptTermsRef = useRef({ value: false });
+  const modalRef = useRef<ModalHandles>(null);
 
   const handleSubmit = useCallback((e: FormEvent) => {
     e.preventDefault();
@@ -15,6 +17,10 @@ function App() {
   const handleAcceptTerms = useCallback(() => {
     acceptTermsRef.current.value = !acceptTermsRef.current.value;
   }, []);
+
+  const handleOpenModal = useCallback(() => {
+    modalRef.current?.openModal;
+  }, []);
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
@@ -24,6 +30,8 @@ function App() {
         </button>
         <button type="submit">Enviar</button>
       </form>
+      <button onClick={handleOpenModal}>Abrir Modal</button>
+      <Modal ref={modalRef} />
     </div>
   );
 }
